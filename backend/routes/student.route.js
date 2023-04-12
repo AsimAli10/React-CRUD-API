323dfc2b117592675cd9626e60a919ba84eb9698
Asim Ali <i190662@nu.edu.pk>
2023-04-12 09:01:56 +0500

    get using name or email

--- backend/routes/student.route.js
+++ backend/routes/student.route.js
@@ -18,6 +18,21 @@ router.route('/').get(async (req, res) => {
   if (!data) return res.status(404).send('No data found.');
   res.json(data);
 })
+/// READ student using email
+router.route('/get-student-byemail').get(async (req, res) => {
+  console.log(req.query.email);
+  const data = await studentSchema.find({"email": req.query.email});
+  if (!data) return res.status(404).send('No data found.');
+  res.json(data);
+})
+
+// READ student using name
+router.route('/get-student-byname').get(async (req, res) => {
+  const data = await studentSchema.find({"name": req.query.name});
+  if (!data) return res.status(404).send('No data found.');
+  res.json(data);
+})
+
 // Get Single Student
 router.route('/edit-student/:id').get(async (req, res) => {
   const data = await studentSchema.findById(req.params.id);
